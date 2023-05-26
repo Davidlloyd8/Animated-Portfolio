c04c87e017849411390a671ded2ee752679b354d
Nwadirioha David <40855998+Davidlloyd8@users.noreply.github.com>
2023-05-26 13:57:45 +0100

    create the sidebar component

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,70 @@
+import { Link, NavLink } from 'react-router-dom'
+import './index.scss'
+import logoD from '../../assets/images/logoD.jpg'
+import logoDD from '../../assets/images/logoD1.jpg'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faLinkedinIn, faTwitter } from '@fortawesome/free-brands-svg-icons'
+
+const Sidebar = () => {
+  return (
+    <div className="nav-bar">
+      <Link className="logo" to="/">
+        <img src={logoD} alt="logo" />
+        <img src={logoDD} className="sub-logo" alt="logo" />
+      </Link>
+      <nav>
+        <NavLink exact="true" activeclassname="active" to="/">
+          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          className="about-link"
+          to="/about"
+        >
+          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          className="contact-link"
+          to="/contact"
+        >
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+        </NavLink>
+      </nav>
+      <ul>
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.linkedin.com/in/nwadirioha-david-86324a20a/"
+          >
+            <FontAwesomeIcon icon={faLinkedinIn} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://github.com/Davidlloyd8"
+          >
+            <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://twitter.com/iamdavidlloyd"
+          >
+            <FontAwesomeIcon icon={faTwitter} color="#4d4d4e" />
+          </a>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default Sidebar
